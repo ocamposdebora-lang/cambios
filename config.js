d392ba947bc3aea981565651a15649d490a0aa2a
ocamposdebora-lang <ocamposdebora@gmail.com>
2026-01-14 18:37:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-const COMPRA_BRL_PYG = 1195;
-const VENTA_BRL_PYG = 1255;
+// Cotización (EDITÁS MANUALMENTE)
+const COMPRA_BRL_PYG = 1195;     // 1 BRL = Gs (Compra)
+const VENTA_BRL_PYG  = 1255;     // 1 BRL = Gs (Venta)
 
-// Fecha y hora EDITADAS MANUALMENTE
+// Fecha y hora de actualización (EDITÁS MANUALMENTE)
 const FECHA_COTIZACION = "13/01/2026";
-const HORA_COTIZACION = "14:56";
+const HORA_COTIZACION  = "18:04"; // formato 24h "18:04"
+
+// WhatsApp destino
+const WHATSAPP_NUM = "595982898734";
